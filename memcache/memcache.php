99852919139f45467b9a780f224ba0989a1db346
yangliu618 <yangliu618@sina.com>
2016-05-30 16:14:33 +0800

    add tools

--- memcache/memcache.php
+++ memcache/memcache.php
@@ -1,9 +1,10 @@
 <?php
-
+//test memcache stats 
 $memcache_obj = new Memcache;
 $memcache_obj->connect('112.124.33.125', 11211);
 $memcache_obj->set('some_key','123456879',0,180);
 $memcache_obj->set('second_key','123456879123456879123456879123456879123456879',0,360);
 $var = $memcache_obj->get(Array('some_key', 'second_key'));
-
+echo '<pre>';
 print_r($var);
+print_r($memcache_obj->getStats());

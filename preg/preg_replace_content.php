df32143041584e29911f8e99c3ee8d1830327b34
klinsmann <klinsmann@cubee.com>
2017-02-21 10:08:44 +0800

    查找内容中的所有非gif格式的图片地址，并且添加水印

---
+++ preg/preg_replace_content.php
@@ -0,0 +1,121 @@
+<?php
+
+$content = '<pre><p>
+	<span style="font-size:18px;line-height:1.5;">英超联赛第13轮，埃弗顿将作客挑战圣徒南安普敦，这是埃弗顿主帅罗纳德-科曼本赛季第一次回到圣玛丽球场。</span> 
+</p>
+<br />
+<p>
+	<span style="font-size:18px;">在上赛季执教圣徒取得欧战席位后科曼选择离开，他在今夏加盟了埃弗顿成，谈到此番重回圣徒的感受，科曼说：“我不知道他们会有怎样的反应，我不能改变人们的观点和南安普顿球迷的感受，我希望他们能尊重我，我们曾经度过了非常好的时光，执教生涯结束后，我将会时常回想起在这里的时光。”</span> 
+</p>
+<p>
+	<span style="font-size:18px;"><img src="http://zqmfcdn.huanhuba.com/app_static/images/championship/upload/780cc5e5b27ab01b6dbfb29055b650af6bcccd41.jpg" alt="" />&nbsp;&nbsp;</span> 
+</p>
+<p>
+	<span style="font-size:18px;"><img src="http://zqmfcdn.huanhuba.com/app_static/images/championship/upload/780cc5e5b27ab01b6dbfb29055b650af6bcccd41.jpEg" alt="" />&nbsp;&nbsp;</span> 
+</p>
+<p>
+	<span style="font-size:18px;"><img src="http://zqmfcdn.huanhuba.com/app_static/images/championship/upload/780cc5e5b27ab01b6dbfb29055b650af6bcccd41.jpg" alt="" />&nbsp;&nbsp;</span> 
+</p>
+<p>
+	<span style="font-size:18px;">英超联赛第13轮，埃弗顿将作客挑战圣徒南安普顿，这是埃弗顿主帅罗纳德-科曼本赛季第一次回到圣玛丽球场。<br />
+</span> 
+</p>
+<p>
+	<span style="font-size:18px;">&nbsp;&nbsp;<img src="http://zqmfcdn.huanhuba.com/app_static/images/championship/upload/adef0889966cc5e0856a789b5dd168c83e0ffbf4.jpeg" alt="" />&nbsp;&nbsp;<br />
+</span> 
+</p>
+<p>
+	<br />
+</p>
+<p style="text-align:center;">
+	<span style="font-size:18px;"><img src="http://cdn.cubegoal.com/app_static/images/championship/upload/461264bd66ff68355a4567d535baaae5152518e3.gif" alt="" /><br />
+</span>
+
+<p>
+	<br />
+</p>
+<p>
+	<br />
+</p>
+<p>
+	<span style="font-size:18px;"><br />
+</span> 
+</p>
+<p>
+	<span style="font-size:18px;"><br />
+</span> 
+</p>
+<p>
+	<span style="font-size:18px;color:#003399;"><strong>“当然我理解不同的观点，不同的反应，我并不害怕被嘘。”</strong></span> 
+</p>
+<p>
+	<span style="font-size:18px;color:#003399;"><strong><br />
+</strong></span> 
+</p>
+<p>
+	<span style="font-size:18px;color:#FFFFFF;background-color:#337FE5;"><strong>足球魔方APP下面为大家带来本场看点</strong></span> 
+</p>
+<p>
+	<br />
+</p>
+<span style="font-size:18px;"><span style="color:#003399;"><strong></strong></span><span style="color:#003399;"><strong>送点狂魔</strong></span><br />
+本赛季埃弗顿已经被判罚3个点球，全部是由中后卫贾吉尔卡送出的。不过，门将斯特克伦博格表现神勇，扑出了其中的2个<br />
+<br />
+<span style="color:#003399;"><strong>交手多零封</strong></span><br />
+在联赛中，埃弗顿曾13次零封南安普敦，而且近2个主场南安普敦均无法进球，本场他们有希望追平零封南安普敦的最多纪录<br />
+<br />
+<strong><span style="color:#003399;">赛程优势</span></strong><br />
+</span> 
+<p>
+	<span style="font-size:18px;">下周中 南安普敦还要在联赛杯中面对英超强队阿森纳，之后还有欧联杯生死战，连续的一周双赛，球队的体力和精力都将受到考验</span> 
+</p>
+<p>
+	<span style="font-size:18px;"><br />
+</span> 
+</p>
+<p>
+	<span style="font-size:18px;"> </span> 
+</p>
+<p style="text-align:center;">
+	<br />
+</p>
+<p style="text-align:center;">
+	<span style="font-size:18px;line-height:27px;"><span style="font-size:24px;color:#FFFFFF;background-color:#003399;"><strong><span style="color:#FFFFFF;"><a href="www.cubegoal.com" target="_blank"><span style="color:#FFFFFF;">点击获取更多足球资讯</span></a></span></strong></span></span><span style="line-height:1.5;"><a href="www.cubegoal.com" target="_blank"><span style="color:#FFFFFF;"></span></a><a href="www.cubegoal.com" target="_blank"><span id="__kindeditor_bookmark_end_114__"></span></a></span> 
+</p>
+<p>
+	<br />
+</p>
+<p style="text-align:center;">
+	<span style="font-size:24px;color:#FFFFFF;background-color:#FF9900;"><strong><span style="color:#FFFFFF;background-color:#003399;"><strong><a href="http://admin.huanhuba.com/www.cubegoal.com" target="_blank"></a><a href="http://www.cubegoal.com" target="_blank"><img src="http://sapp.huanhuba.com/images/championship/upload/02e9d1d215253784f1e883c586dcabc916af3019.jpg" width="487" height="271" title="足球魔方" alt="足球魔方" /></a></strong></span></strong></span> 
+</p>
+<p>
+	<br />
+</p></pre>';
+
+
+/*
+//分析并找出：数据库中含有的图片连接地址
+$content = file_get_contents('bigdata.sql');
+preg_match_all("/<img([^>]*)\s*src=\\\('|\")([^'\"]+)\\\('|\")/", $content,$matches); //带引号，数据库中导出的sql，要对引号和\进行转义。
+foreach($matches[3] as $row){
+	if(strpos($row,'championship')){
+		continue;
+	}
+	echo $row.PHP_EOL;
+}
+//print_r($matches[3]);
+die;
+*/
+
+//preg_match_all("/<img([^>]*)\s*src=('|\")([^'\"]+)('|\")/", $content,$matches);//带引号
+
+$search_words = ['zqmfcdn.huanhuba.com','sapp.huanhuba.com'];
+$replace_words = ['cdn.cubegoal.com','cdn.cubegoal.com/app_static'];
+$content = str_replace($search_words,$replace_words,$content);
+
+$patterns = "/(<img([^>]*)\s*src=)('|\")([^'\"]+)(jpg|jpeg|bmp|png|webp)('|\")/i";  //查找内容中的所有非gif格式的图片地址，并且添加水印
+//$replace  = "*** $1 1,  $2 2,  $3 3,  $4 4, $5 5, $6 6 ***";  //可以在替换的位置打印每个（）号变量的含义 $2=img后面紧跟的括号
+$replace  = "$1$3$4$5".'?x-oss-process=image/resize,w_400/watermark,image_Y3ViZWdvYWwvc3RhdGljL2ltZy9jdWJlZ29hbF93YXRlcm1hcmsucG5n,g_se,x_10,y_10'."$6";
+$content  = preg_replace($patterns, $replace, $content);
+
+var_dump($content);
